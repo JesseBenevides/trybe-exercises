4224dc90d99fae7ab2c8f15a2d194d7bccaf1509
Jesse Benevides <jessebenevides@gmail.com>
2021-07-30 19:20:24 -0300

    Ajustando exercícios

--- 01-fundamentos-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-e-algoritmos/exercicio_02.js
+++ 01-fundamentos-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-e-algoritmos/exercicio_02.js
@@ -2,7 +2,7 @@
 //faça o mesmo que o exercício 1, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
 let n = 5
-let square = ''
+let triangle = ''
 
 for (let i = 0; i < n; i += 1) {
   square += '*'
--- 01-fundamentos-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-e-algoritmos/exercicio_03.js
+++ 01-fundamentos-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-e-algoritmos/exercicio_03.js
@@ -1,17 +1,26 @@
 //EXERCÍCIO 3
 //Agora inverta o lado do triângulo
 
-let n = 5
-let square = ''
+//RESOLUÇÃO USANDO FUNÇÃO REPEAT
+// let n = 5
+// let triangle = ''
 
-// for (let i = n - 1; i > 0; i -= 1) {
-//   square += ' '
+// for (let i = 1; i <= n; i += 1) {
+//   square = ' '.repeat(n - i)
+//   square += '*'.repeat(i)
+//   console.log(square)
 // }
-// square += '*'
-// console.log(square)
 
-for (let i = 1; i <= n; i += 1) {
-  square = ' '.repeat(n - i)
-  square += '*'.repeat(i)
-  console.log(square)
+//RESOLUÇÃO SEM O REPEAT
+let n = 5
+
+for (let i = 1; i <= n; i++) {
+  let triangle = ''
+  for (let j = 0; j < n - i; j++) {
+    triangle += ' '
+  }
+  for (let k = 0; k < i; k++) {
+    triangle += '*'
+  }
+  console.log(triangle)
 }
