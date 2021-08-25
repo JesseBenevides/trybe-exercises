9c33276e40cc5b4bfb6cde9d2b93ed0a146fda1a
Jesse Benevides <jessebenevides@gmail.com>
2021-08-25 12:28:03 -0300

    Adiciona bibliotecas e frameworks no form do dia 6.1

---
+++ 01-fundamentos-web/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,48 @@
+import './node_modules/just-validate/dist/js/just-validate.js'
+
+function createStateOptions() {
+  const stateList = [
+    'AC',
+    'AL',
+    'AM',
+    'AP',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MG',
+    'MS',
+    'MT',
+    'PA',
+    'PB',
+    'PE',
+    'PI',
+    'PR',
+    'RJ',
+    'RN',
+    'RO',
+    'RR',
+    'RS',
+    'SC',
+    'SE',
+    'SP',
+    'TO'
+  ]
+
+  const stateForm = document.getElementById('state')
+
+  for (let state of stateList) {
+    let stateOption = document.createElement('option')
+
+    stateOption.innerText = state
+    stateOption.value = state
+
+    stateForm.appendChild(stateOption)
+  }
+}
+
+window.onload = function () {
+  createStateOptions()
+}
