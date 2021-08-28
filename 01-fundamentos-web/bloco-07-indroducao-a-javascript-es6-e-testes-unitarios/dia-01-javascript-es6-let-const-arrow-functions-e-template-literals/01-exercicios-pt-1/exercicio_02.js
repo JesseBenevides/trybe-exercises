a9a0d98461cb9896235d1917ecfec839dc7f04ae
Jesse Benevides <jessebenevides@gmail.com>
2021-08-28 00:28:05 -0300

    Adiciona exercicios do dia 7.1

---
+++ 01-fundamentos-web/bloco-07-indroducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/01-exercicios-pt-1/exercicio_02.js
@@ -0,0 +1,7 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// const sortArray = array => array.sort();
+
+oddsAndEvens.sort();
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
