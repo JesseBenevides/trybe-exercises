315a9b7eb79157f0aa6e7057819a5c03b93112c6
Jesse Benevides <jessebenevides@gmail.com>
2021-11-01 20:29:19 -0300

    Tests 1 - verifica  inputs com getByLabelText

--- 02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/00-rtl-first-tests/src/App.test.js
+++ 02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/00-rtl-first-tests/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verifica se existe o input do tipo email', () => {
+  // Acessar os Elementos da tela
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputEmail = screen.getByLabelText(/email/i);
+
+  // Realizar os testes
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email')
 });
+
