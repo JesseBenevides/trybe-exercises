708477774318569be8106dd4c833196cb46ec7ae
Jesse Benevides <jessebenevides@gmail.com>
2022-02-08 19:18:26 -0300

    Adiciona Desafio 6

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-um-motor-javascript/exercicios/velocidade.js
@@ -0,0 +1,24 @@
+const readline = require('readline-sync');
+
+function getDistance() {
+  const distance = readline.questionInt('Qual a distância percorrida(em metros)? ');
+  return distance;
+}
+
+function getTime() {
+  const time = readline.questionInt('Quanto tempo dutou o percurso(em segundos)? ');
+  return time;
+}
+function calculateSpeed(distance, time) {
+  return distance / time;
+}
+
+function main() {
+  const distance = getDistance();
+  const time = getTime();
+  const speed = calculateSpeed(distance, time).toFixed(2);
+
+  console.log(`A velocidade média do carro é ${speed}m/s`);
+}
+
+main();
