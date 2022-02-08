4af8ae18591b5ad912d381515293ffa09b5a23f6
Jesse Benevides <jessebenevides@gmail.com>
2022-02-08 18:47:25 -0300

    Adiciona Desafio 5

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-um-motor-javascript/exercicios/imc.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-um-motor-javascript/exercicios/imc.js
@@ -1,11 +1,29 @@
 const readline = require('readline-sync');
 
-const weight = readline.question('Qual seu peso? ');
-const height = readline.question('Qual sua altura? ');
+const weight = readline.questionFloat('Qual seu peso? ');
+const height = readline.questionFloat('Qual sua altura? ');
 
 function getIMC(height, weight) {
   return weight / (height * height);
 }
 
-const imc = getIMC(height, weight);
-console.log(`Seu IMC é ${imc}`);
+function getCategory(imc) {
+  let category;
+  if (imc < 18.5 ) category = 'Abaixo do peso (magreza)';
+  else if (imc <= 24.9) category = 'Peso normal';
+  else if (imc <= 29.9) category = 'Acima do peso (sobrepeso)';
+  else if (imc <= 34.9) category = 'Obesidade grau I';
+  else if (imc <= 39.9) category = 'Obesidade grau II';
+  else category = 'Obesidade graus III e IV';
+
+  return category;
+}
+
+function main() {
+  const imc = getIMC(height, weight).toFixed(2);
+  const category = getCategory(imc);
+  console.log(`Seu IMC é ${imc}`);
+  console.log(`Sua  situação é ${category}`);
+}
+
+main();
