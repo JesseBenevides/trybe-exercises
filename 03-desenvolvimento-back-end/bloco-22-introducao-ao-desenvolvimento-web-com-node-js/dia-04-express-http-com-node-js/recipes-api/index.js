897643df230090f82bd29ea05c9bfbb859ff8df0
Jesse Benevides <jessebenevides@gmail.com>
2022-02-11 18:20:18 -0300

    Adiciona a rota para fazer o get de uma receita por id utilizando parametro

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-http-com-node-js/recipes-api/index.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-http-com-node-js/recipes-api/index.js
@@ -21,6 +21,15 @@ app.get('/recipes', function (req, res) {
   res.json(recipes);
 });
 
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
 app.get('/drinks', function (req, res) {
   res.json(drinks);
 });
