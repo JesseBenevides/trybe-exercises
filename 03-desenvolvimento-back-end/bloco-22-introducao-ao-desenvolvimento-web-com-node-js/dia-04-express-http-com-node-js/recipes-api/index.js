de65f545d9f9b468acdbcbd4c05bb4f7388953a1
Jesse Benevides <jessebenevides@gmail.com>
2022-02-11 18:22:39 -0300

    Adiciona a rota para fazer o get de um drink por id utilizando parametro

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-http-com-node-js/recipes-api/index.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-http-com-node-js/recipes-api/index.js
@@ -34,6 +34,15 @@ app.get('/drinks', function (req, res) {
   res.json(drinks);
 });
 
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((d) => d.id === parseInt(id));
+
+  if (!drink) return res.status(404).json({ message: 'Drink not found!'});
+
+  res.status(200).json(drink);
+});
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
