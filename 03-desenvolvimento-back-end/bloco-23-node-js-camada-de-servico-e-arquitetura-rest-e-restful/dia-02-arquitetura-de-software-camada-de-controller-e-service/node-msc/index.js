d9d9ff83ab897aac2a39d8b9d7af3feebc0311c4
Jesse Benevides <jessebenevides@gmail.com>
2022-02-18 15:08:00 -0300

    Refatora o index para utilizar a camada de controllers

--- 03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/node-msc/index.js
+++ 03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/node-msc/index.js
@@ -1,35 +1,15 @@
 const express = require('express');
-const Author = require('./services/Author');
+const Author = require('./controllers/Author');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/authors', async (_req, res) => {
-  const authors = await Author.getAll();
+app.get('/authors', Author.getAll);
 
-  return res.status(200).json(authors);
-});
+app.get('/authors/:id', Author.findById);
 
-app.get('/authors/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const author = await Author.findById(id);
-
-  if (!author) return res.status(404).json({ message: 'Not found' });
-
-  return res.status(200).json(author);
-});
-
-app.post('/authors', async (req, res) => {
-  const { first_name, middle_name, last_name } = req.body;
-
-  const author = await Author.createAuthor(first_name, middle_name, last_name);
-
-  if (!author) return res.status(400).json({ message: 'Dados inválidos' });
-
-  return res.status(201).json(author);
-});
+app.post('/authors', Author.create);
 
 const PORT = 3000;
 
